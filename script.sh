f698cfe55ff6177573a799fb6a39dca61f2d67f5
Gerard Carrillo <gery11@elpuig.xeill.net>
2020-11-27 17:08:11 +0100

    first commit

---
+++ script.sh
@@ -0,0 +1,10 @@
+apt install apache2 -y
+apt install mysql-server -y
+apt install php-mysql -y
+apt install php
+
+
+mysql -e "CREATE DATABASE BASEDEDATOS"
+mysql -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin'"
+mysql -e "GRANT ALL PRIVILEGES ON BASEDEDATOS.* TO 'admin'@'localhost; FLUSH PRIVILEGES"
+mysql -e "CREATE TABLE BASEDEDATOS.TAULA1(descripcio TEXT, prioritat TEXT)"
